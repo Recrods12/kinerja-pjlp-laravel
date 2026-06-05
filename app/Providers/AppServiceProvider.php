df7910488bb780b723846c706aca418e66eb2d86
Records12 <jumatk321@gmail.com>
2026-06-05 11:44:34 +0700

    Force Indonesian locale and isolate print area

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,7 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Carbon::setLocale(config('app.locale', 'id'));
+        App::setLocale('id');
+        config(['app.locale' => 'id']);
+        Carbon::setLocale('id');
         setlocale(LC_TIME, 'id_ID.UTF-8', 'Indonesian_indonesia.1252', 'id_ID', 'id');
     }
 }
